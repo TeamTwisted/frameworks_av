9d7f721cbe6f8b763d5e101c9450e782664d910c
Pavan Chikkala <pavanc@codeaurora.org>
2015-01-09 11:41:50 +0530

    audiopolicy: Add DEVICE_IN_COMMUNICATION for VOIP
    - On A family, Only DEVICE_IN_COMMUNICATION is supported
      by voip_tx profile. getDeviceForInputSource should
      always return DEVICE_IN_COMMUNICATION to load
      voip_tx profile
    
    Change-Id: I3bb2fb70a89eadc35c2ced0c7f717ec15120bfe7

--- services/audiopolicy/enginedefault/src/Engine.cpp
+++ services/audiopolicy/enginedefault/src/Engine.cpp
@@ -623,6 +623,9 @@ audio_devices_t Engine::getDeviceForInputSource(audio_source_t inputSource) cons
     break;
 
     case AUDIO_SOURCE_VOICE_COMMUNICATION:
+#ifdef LEGACY_ALSA_AUDIO
+        device = AUDIO_DEVICE_IN_COMMUNICATION;
+#else
         // Allow only use of devices on primary input if in call and HAL does not support routing
         // to voice call path.
         if ((getPhoneState() == AUDIO_MODE_IN_CALL) &&
@@ -660,6 +663,7 @@ audio_devices_t Engine::getDeviceForInputSource(audio_source_t inputSource) cons
             }
             break;
         }
+#endif
         break;
 
     case AUDIO_SOURCE_VOICE_RECOGNITION:
